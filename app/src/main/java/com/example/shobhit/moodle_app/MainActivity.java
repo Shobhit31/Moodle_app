dbb0df59ad5e5a310d65d4ff8f03dfbbb182b824
abhi22parashar <abhi22parashar@gmail.com>
2016-02-24 15:22:24 +0530

    Slight progress

--- app/src/main/java/com/example/shobhit/moodle_app/MainActivity.java
+++ app/src/main/java/com/example/shobhit/moodle_app/MainActivity.java
@@ -1,10 +1,27 @@
 package com.example.shobhit.moodle_app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
